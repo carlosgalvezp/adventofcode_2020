86d7e20f3befd07435c90db53001474a455b29b6
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-12-22 19:35:33 +0100

    Solve Day 2 Part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,32 +1,75 @@
 use std::env;
 use std::fs;
 
+struct LineInfo
+{
+    min: i32,
+    max: i32,
+    letter: char,
+    password: String,
+}
+
+fn get_line_info(line: &str) -> LineInfo{
+    let space_tokens: Vec<&str> = line.split(' ').collect();
+    let size_part : &str = space_tokens[0];
+    let letter_part : &str = space_tokens[1];
+    let password : &str = space_tokens[2];
+
+    let min_max: Vec<&str> = size_part.split('-').collect();
+    let min = min_max[0].parse::<i32>().unwrap();
+    let max = min_max[1].parse::<i32>().unwrap();
+
+    let letter : char  = letter_part.chars().next().unwrap();
+
+    return LineInfo { min, max, letter, password: password.to_owned()};
+}
+
 fn day1(contents: String)
 {
     let lines = contents.lines();
     let mut valid_passwords = 0;
 
     for line in lines{
-        let space_tokens: Vec<&str> = line.split(' ').collect();
-        let size_part : &str = space_tokens[0];
-        let letter_part : &str = space_tokens[1];
-        let password : &str = space_tokens[2];
-
-        let min_max_letters_tokens: Vec<&str> = size_part.split('-').collect();
-        let min_letters = min_max_letters_tokens[0].parse::<i32>().unwrap();
-        let max_letters = min_max_letters_tokens[1].parse::<i32>().unwrap();
-
-        let letter : char  = letter_part.chars().next().unwrap();
+        let line_info = get_line_info(line);
 
         let mut count = 0;
 
-        for c in password.chars(){
-            if c == letter{
+        for c in line_info.password.chars(){
+            if c == line_info.letter{
                 count += 1;
             }
         }
 
-        if count >= min_letters && count <= max_letters{
+        if (count >= line_info.min) && (count <= line_info.max){
+            valid_passwords += 1;
+        }
+    }
+
+    println!("Number of valid passwords: {}", valid_passwords);
+}
+
+fn day2(contents: String)
+{
+    let lines = contents.lines();
+    let mut valid_passwords = 0;
+
+    for line in lines{
+        let line_info = get_line_info(line);
+
+        let mut count = 0;
+
+        let idx_min = (line_info.min - 1) as usize;
+        let idx_max = (line_info.max - 1) as usize;
+
+        if line_info.password.chars().nth(idx_min).unwrap() == line_info.letter{
+            count += 1;
+        }
+
+        if line_info.password.chars().nth(idx_max).unwrap() == line_info.letter{
+            count += 1;
+        }
+
+        if count == 1{
             valid_passwords += 1;
         }
     }
@@ -36,8 +79,17 @@ fn day1(contents: String)
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let input = &args[1];
-    let contents = fs::read_to_string(input).expect("Something went wrong reading the file");
+    let option : i32 = args[1].parse().unwrap();
+    let fname = &args[2];
+    let contents = fs::read_to_string(fname).expect("Something went wrong reading the file");
 
-    day1(contents);
+    if option == 1{
+        day1(contents);
+    }
+    else if option == 2{
+        day2(contents);
+    }
+    else{
+        panic!("Wrong option!");
+    }
 }
