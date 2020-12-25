5ae183267d64025294e5778da68892b36dc425f3
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-12-25 17:46:32 +0100

    Solve Day 6 Part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -18,7 +18,29 @@ fn day1(contents: String){
 }
 
 fn day2(contents: String){
+    let group_answers = contents.split("\n\n").map(|x| x.to_owned());
 
+    let mut n_yes_answers = 0;
+
+    for mut group_answer in group_answers{
+        let n_people_in_group = group_answer.lines().count();
+
+        group_answer = group_answer.replace("\n", "");
+        let mut group_sorted: Vec<char> = group_answer.chars().collect();
+        group_sorted.sort();
+
+        let mut all_yes: Vec<char> = Vec::new();
+
+        for i in 0..group_sorted.len() - (n_people_in_group - 1){
+            if group_sorted[i] == group_sorted[i + n_people_in_group - 1]{
+                all_yes.push(group_sorted[i]);
+            }
+        }
+
+        n_yes_answers += all_yes.len();
+    }
+
+    println!("Number of ALL yes answers: {}", n_yes_answers);
 }
 
 fn main() {
