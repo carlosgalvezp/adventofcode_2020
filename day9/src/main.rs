f21dceee163a7e1a22bb24575e377912f73f900e
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-12-27 10:36:39 +0100

    Solve Day 9 Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,13 +1,17 @@
 use std::env;
 use std::fs;
 
-fn day1(contents: String) {
+fn get_numbers(contents: String) -> Vec<i64> {
     let mut numbers = Vec::new();
 
     for line in contents.lines(){
         numbers.push(line.parse::<i64>().unwrap());
     }
 
+    return numbers;
+}
+
+fn get_invalid_number(numbers: &Vec<i64>) -> i64{
     for i in 25..numbers.len(){
         let mut found = false;
         for j in i-25..i{
@@ -23,13 +27,44 @@ fn day1(contents: String) {
         }
 
         if !found{
-            println!("The first number that doesn't follow the rule is {}", numbers[i]);
-            break;
+            return numbers[i];
         }
     }
+    panic!("Could not find number");
+}
+
+fn day1(contents: String) {
+    let numbers = get_numbers(contents);
+    let invalid_number = get_invalid_number(&numbers);
+    println!("Invalid number: {}", invalid_number);
 }
 
 fn day2(contents: String) {
+    let numbers = get_numbers(contents);
+    let invalid_number = get_invalid_number(&numbers);
+
+    for i in 0..numbers.len() - 1{
+        for j in i+1..numbers.len(){
+            let mut sum = 0;
+
+            let mut min_number = i64::MAX;
+            let mut max_number = 0;
+            for k in i..j+1{
+                sum += numbers[k];
+
+                min_number = std::cmp::min(min_number, numbers[k]);
+                max_number = std::cmp::max(max_number, numbers[k]);
+            }
+
+            if sum == invalid_number{
+                println!("Day 2 solution: {}", min_number + max_number);
+                return;
+            }
+            if sum > invalid_number{
+                break;
+            }
+        }
+    }
 }
 
 fn main() {
