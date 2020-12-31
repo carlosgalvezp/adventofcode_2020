ed8d40113e13b3b7b925dbcdbedde551280a9700
Carlos Gálvez <carlosgalvezp@gmail.com>
2020-12-31 20:42:24 +0100

    Solve Day 19 Part 2

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use regex::Regex;
 use std::collections::HashMap;
 
-fn get_rule(rules_str: &str, rules_cache: &mut HashMap<i32, String>, rule_number_query: i32) -> String{
+fn get_rule(rules_str: &str, rules_cache: &mut HashMap<i32, String>, rule_number_query: i32, is_part2: bool) -> String{
     // First, check in cache
     if rules_cache.contains_key(&rule_number_query) {
         return rules_cache[&rule_number_query].clone();
@@ -15,23 +15,41 @@ fn get_rule(rules_str: &str, rules_cache: &mut HashMap<i32, String>, rule_number
         let rule_number = split_colon[0].parse::<i32>().unwrap();
 
         if rule_number == rule_number_query{
-            let rule_contents = split_colon[1].trim();
+            let mut output = "".to_string();
 
-            let mut output = "(".to_string();
-
-            for subrule in rule_contents.split(" "){
-                if subrule == "|"{
-                    output += "|";
+            if is_part2 && ((rule_number == 8) || (rule_number == 11)){
+                if rule_number == 8 {
+                    let rule_42 = &get_rule(rules_str, rules_cache, 42, false);
+                    output = format!("({})+", rule_42);
                 }
-                else if subrule.parse::<i32>().is_ok(){
-                    output += &get_rule(rules_str, rules_cache, subrule.parse::<i32>().unwrap());
+                else if rule_number == 11 {
+                    let rule_42 = &get_rule(rules_str, rules_cache, 42, is_part2);
+                    let rule_31 = &get_rule(rules_str, rules_cache, 31, is_part2);
+                    let mut rule_11_variants = Vec::new();
+                    for i in 1..5{
+                        rule_11_variants.push(format!("(({rule_42}){{{n}}}({rule_42}{rule_31})?({rule_31}){{{n}}})", rule_42=rule_42, rule_31=rule_31, n=i));
+                    }
+                    output = rule_11_variants.join("|");
                 }
-                else{
-                    output += subrule.trim_matches('"');
+            }
+            else{
+                for subrule in split_colon[1].trim().split(" "){
+                    if subrule == "|"{
+                        output += "|";
+                    }
+                    else if subrule.parse::<i32>().is_ok(){
+                        output += &get_rule(rules_str, rules_cache, subrule.parse::<i32>().unwrap(), is_part2);
+                    }
+                    else{
+                        output += subrule.trim_matches('"');
+                    }
                 }
             }
 
-            output += ")";
+            if output.len() > 1{
+                output = format!("({})", output);
+            }
+
             rules_cache.insert(rule_number_query, output.clone());
             return output;
         }
@@ -40,14 +58,14 @@ fn get_rule(rules_str: &str, rules_cache: &mut HashMap<i32, String>, rule_number
     panic!("Could not find rule {}", rule_number_query);
 }
 
-fn day1(contents: String){
+fn get_solution(contents: String, is_part2: bool){
     let groups: Vec<String> = contents.split("\n\n").map(|x| x.to_owned()).collect();
     let rules_str = &groups[0];
     let message = &groups[1];
 
     // Get Rule 0
     let mut rules_cache : HashMap<i32, String> = HashMap::new();
-    let rule_0 = get_rule(rules_str, &mut rules_cache, 0);
+    let rule_0 = get_rule(rules_str, &mut rules_cache, 0, is_part2);
 
     // Determine number of valid messages
     let regex_rule_0 = "^".to_owned() + &rule_0 + "$";
@@ -60,11 +78,15 @@ fn day1(contents: String){
         }
     }
 
-    println!("Part 1 solution: {}", output);
+    println!("Solution: {}", output);
+}
+
+fn day1(contents: String){
+    get_solution(contents, false);
 }
 
 fn day2(contents: String){
-
+    get_solution(contents, true);
 }
 
 fn main() {
